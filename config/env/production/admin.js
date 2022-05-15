2fa553a3de60966d75cb3dfef1eac8428e3dfa4f
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-05-15 22:32:53 +0200

    revert

--- config/env/production/admin.js
+++ config/env/production/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env("ADMIN_JWT_SECRET", "220777d63ce9d4bb528f0238ef9ce48a"),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
     salt: env("API_TOKEN_SALT"),
