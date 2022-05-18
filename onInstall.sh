e3132b3f9f220a67772722bba2d9bc32aa76729c
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-05-18 09:01:25 +0200

    add sh files

---
+++ onInstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "npm install and build project"
+npm install && npm run build
+echo "add ecosystem to pm2"
+ pm2 start ecosystem.config.js && pm2 save --force
---
+++ onRestart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "restart strapi-server"
+pm2 restart strapi-server
