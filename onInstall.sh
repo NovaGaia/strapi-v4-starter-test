683993662858e0e2d1979502abbe5b1c2bae0278
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-05-18 10:17:07 +0200

    chaged to pm2 save

--- onInstall.sh
+++ onInstall.sh
@@ -3,4 +3,4 @@
 echo "npm install and build project"
 npm install && npm run build
 echo "add ecosystem to pm2"
- pm2 start ecosystem.config.js && pm2 update
+ pm2 start ecosystem.config.js && pm2 save
